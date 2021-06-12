c58e24953e7a598b2917a9c4acab61810b3ae370
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-06-13 02:57:37 +0530

    Image carousel added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
-});
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items: 4,
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel();
+// });
+// var owl = $('.owl-carousel');
+// owl.owlCarousel({
+//     items: 4,
+//     loop: true,
+//     margin: 10,
+//     autoplay: true,
+//     autoplayTimeout: 3000,
+//     autoplayHoverPause: true
+
+// });
+// $('.play').on('click', function () {
+//     owl.trigger('play.owl.autoplay', [1000])
+// })
+// $('.stop').on('click', function () {
+//     owl.trigger('stop.owl.autoplay')
+// })
+
+// SLIDER
+$('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    items: 1,
     loop: true,
-    margin: 10,
+    autoplayHoverPause: false,
     autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true
-    
-});
-$('.play').on('click', function () {
-    owl.trigger('play.owl.autoplay', [1000])
-})
-$('.stop').on('click', function () {
-    owl.trigger('stop.owl.autoplay')
+    smartSpeed: 1000,
 })
