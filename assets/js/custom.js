8602fbd3e4817db1cb7ae9e75c6d06fd6a3ad5ce
koushikpuppala <koushikpuppala203@gmail.com>
2021-07-20 12:24:15 +0530

    updated axios

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,31 +1,56 @@
+/** @format */
 
-  $(function () {
+$(function () {
+	// MENU
+	$('.navbar-collapse a').on('click', function () {
+		$('.navbar-collapse').collapse('hide')
+	})
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
+	// AOS ANIMATION
+	AOS.init({
+		disable: 'mobile',
+		duration: 800,
+		anchorPlacement: 'center-bottom',
+	})
 
-    // AOS ANIMATION
-    AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
-    });
+	// SMOOTHSCROLL NAVBAR
+	$(function () {
+		$('.navbar a, .hero-text a').on('click', function (event) {
+			var $anchor = $(this)
+			$('html, body')
+				.stop()
+				.animate(
+					{
+						scrollTop: $($anchor.attr('href')).offset().top - 49,
+					},
+					1000
+				)
+			event.preventDefault()
+		})
+	})
+})
 
+const createUser = (user) => {
+	axios
+		.post('https://backend-iiitr.puppalakoushik.repl.co/contact', user)
+		.then((response) => {
+			const addedUser = response.data
+			appendToDOM([addedUser])
+		})
+		.catch((error) => console.error(error))
+}
 
-    // SMOOTHSCROLL NAVBAR
-    $(function() {
-      $('.navbar a, .hero-text a').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });    
-  });
+const form = document.querySelector('form')
 
+const formEvent = form.addEventListener('submit', (event) => {
+	event.preventDefault()
 
-    
+	const Name = document.getElementById('Name').value
+	const Email = document.getElementById('Email').value
+	const Phone = document.getElementById('Phone').value
+	const Subject = document.getElementById('Subject').value
+	const Message = document.getElementById('Message').value
 
+	const user = { Name, Email, Phone, Subject, Message }
+	createUser(user)
+})
